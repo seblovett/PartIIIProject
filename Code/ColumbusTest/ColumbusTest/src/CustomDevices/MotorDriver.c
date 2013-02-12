29ae4969dabc7efd5a612749a70b59711bdb25bd
Henry Lovett <hl13g10@soton.ac.uk>
2013-02-12 20:12:38 +0000

    Got PWM working on both lines.

--- Code/ColumbusTest/ColumbusTest/src/CustomDevices/MotorDriver.c
+++ Code/ColumbusTest/ColumbusTest/src/CustomDevices/MotorDriver.c
@@ -21,7 +21,7 @@ static void local_start_highfreq_clock(void)
             .pll_freq = 1,        // Set to 1 for VCO frequency range 80-180MHz, set to 0 for VCO frequency range 160-240Mhz.
   };
   // Switch main clock to Osc0.
-  pcl_switch_to_osc(PCL_OSC0, FOSC0, OSC0_STARTUP);
+//  pcl_switch_to_osc(PCL_OSC0, FOSC0, OSC0_STARTUP);
 
   /* Setup PLL0 on Osc0, mul=7 ,no divisor, lockcount=16, ie. (16Mhzx7)/(div2) = 56MHz output */
   scif_pll_setup(SCIF_PLL0, &opt); // lockcount in main clock for the PLL wait lock
@@ -38,10 +38,10 @@ static void pwm_start_gc(void)
                 SCIF_GCCTRL_PLL0,
                 AVR32_SCIF_GC_NO_DIV_CLOCK,
                 0);
-// 	scif_gc_setup(	AVR32_SCIF_GCLK_PWM,
-// 					PCL_OSC0,
-// 					AVR32_SCIF_GC_NO_DIV_CLOCK,
-// 					0);
+//   scif_gc_setup(  AVR32_SCIF_GCLK_PWM,
+// 				  SCIF_GCCTRL_OSC0,
+// 				  AVR32_SCIF_GC_NO_DIV_CLOCK,
+// 				  0);
   // Now enable the generic clock
   scif_gc_enable(AVR32_SCIF_GCLK_PWM);
 }
@@ -75,53 +75,57 @@ void Motor_Init()
 											{0}, // ccnt
 											{0}, // dt
 											{0}};// dtupd  ;  One channel config.
-		unsigned int channel_id;
+	unsigned int channel_id;
+
+	// Start PLL for PWM
+	local_start_highfreq_clock();
+	// Start Enable Generic Clock with PLL as source clock
+	pwm_start_gc();
 	
-		// Start Main Clock On external 16MHz Oscillator
-		// Start PLL for PWM
-		local_start_highfreq_clock();
-		// Start Enable Generic Clock with PLL as source clock
-		pwm_start_gc();
+	channel_id = 1;//EXAMPLE_PWM_CHANNEL_ID;
+// 	gpio_enable_module_pin(EXAMPLE_PWM_L_PIN, EXAMPLE_PWM_L_FUNCTION);
+// 	gpio_enable_module_pin(EXAMPLE_PWM_H_PIN, EXAMPLE_PWM_H_FUNCTION);
+// 	gpio_enable_module_pin(M0_PWM_H_PIN, M0_PWM_H_FUNCTION);
+// 	gpio_enable_module_pin(AVR32_PIN_PB10, AVR32_PWM_PWMH_1_1_FUNCTION); //PWM1 Low
+	gpio_enable_module_pin(M0_PWM_H_PIN, M0_PWM_H_FUNCTION);
+	gpio_enable_module_pin(M1_PWM_H_PIN, M1_PWM_H_FUNCTION); //PWM1 Low
+	//gpio_enable_module_pin(M1_PWM_H_PIN, M1_PWM_H_FUNCTION);
+	// PWM controller configuration.
+	pwm_opt.diva = AVR32_PWM_DIVA_CLK_OFF;
+	pwm_opt.divb = AVR32_PWM_DIVB_CLK_OFF;
+	pwm_opt.prea = AVR32_PWM_PREA_CCK;
+	pwm_opt.preb = AVR32_PWM_PREB_CCK;
 	
-		channel_id = EXAMPLE_PWM_CHANNEL_ID;
-		gpio_enable_module_pin(EXAMPLE_PWM_L_PIN, EXAMPLE_PWM_L_FUNCTION);
-		gpio_enable_module_pin(EXAMPLE_PWM_H_PIN, EXAMPLE_PWM_H_FUNCTION);
-		// PWM controller configuration.
-		pwm_opt.diva = AVR32_PWM_DIVA_CLK_OFF;
-		pwm_opt.divb = AVR32_PWM_DIVB_CLK_OFF;
-		pwm_opt.prea = AVR32_PWM_PREA_CCK;
-		pwm_opt.preb = AVR32_PWM_PREB_CCK;
+	pwm_opt.fault_detection_activated = false;
+	pwm_opt.sync_channel_activated    = true;
+	pwm_opt.sync_update_channel_mode  = PWM_SYNC_UPDATE_MANUAL_WRITE_MANUAL_UPDATE;
+	pwm_opt.sync_channel_select[0]    = false;
+	pwm_opt.sync_channel_select[1]    = false;
+	pwm_opt.sync_channel_select[2]    = false;
+	pwm_opt.sync_channel_select[3]    = false;
+	pwm_opt.cksel                     = PWM_CKSEL_GCLK;
+	pwm_init(&pwm_opt);
 	
-		pwm_opt.fault_detection_activated = false;
-		pwm_opt.sync_channel_activated    = true;
-		pwm_opt.sync_update_channel_mode  = PWM_SYNC_UPDATE_MANUAL_WRITE_MANUAL_UPDATE;
-		pwm_opt.sync_channel_select[0]    = true;
-		pwm_opt.sync_channel_select[1]    = true;
-		pwm_opt.sync_channel_select[2]    = false;
-		pwm_opt.sync_channel_select[3]    = false;
-		pwm_opt.cksel                     = PWM_CKSEL_GCLK;
-		pwm_init(&pwm_opt);
+	// Update the period
+	pwm_update_period_value(10);
 	
-		// Update the period
-		pwm_update_period_value(10);
+	// Channel configuration
+	pwm_channel.CMR.dte   = 0;        // Enable Deadtime for complementary Mode
+	pwm_channel.CMR.dthi  = 0;        // Deadtime Inverted on PWMH
+	pwm_channel.CMR.dtli  = 0;        // Deadtime Not Inverted on PWML
+	pwm_channel.CMR.ces   = 0;        // 0/1 Channel Event at the End of PWM Period
+	pwm_channel.CMR.calg  = PWM_MODE_LEFT_ALIGNED;       // Channel mode.
+	pwm_channel.CMR.cpol  = PWM_POLARITY_HIGH;            // Channel polarity.
+	pwm_channel.CMR.cpre  = AVR32_PWM_CPRE_CCK;           // Channel prescaler.
+	pwm_channel.cdty      = 100;       // Channel duty cycle, should be < CPRD.
+	pwm_channel.cprd      = 200;       // Channel period.
 	
-		// Channel configuration
-		pwm_channel.CMR.dte   = 0;        // Enable Deadtime for complementary Mode
-		pwm_channel.CMR.dthi  = 0;        // Deadtime Inverted on PWMH
-		pwm_channel.CMR.dtli  = 0;        // Deadtime Not Inverted on PWML
-		pwm_channel.CMR.ces   = 0;        // 0/1 Channel Event at the End of PWM Period
-		pwm_channel.CMR.calg  = PWM_MODE_LEFT_ALIGNED;       // Channel mode.
-		pwm_channel.CMR.cpol  = PWM_POLARITY_LOW;            // Channel polarity.
-		pwm_channel.CMR.cpre  = AVR32_PWM_CPRE_CCK;           // Channel prescaler.
-		pwm_channel.cdty      = 100;       // Channel duty cycle, should be < CPRD.
-		pwm_channel.cprd      = 200;       // Channel period.
-	
-		// With these settings, the output waveform period will be :
-		// (56MHz)/20 == 2.8MHz == (MCK/prescaler)/period, with MCK == 56MHz,
-		// prescaler == 1, period == 20.
 
 	pwm_channel_init(channel_id, &pwm_channel); // Set channel configuration to channel 0
-	pwm_start_channels((1 << channel_id));  // Start channel 0 & 1.
+	//pwm_start_channels((1 << channel_id));  // Start channel 0 & 1.
+	channel_id = 2; 
+	pwm_channel_init(channel_id, &pwm_channel); // Set channel configuration to channel 0
+	//pwm_start_channels((1 << channel_id));  // Start channel 0 & 1.
 }
 
 void Motor_Go(int Direction)
