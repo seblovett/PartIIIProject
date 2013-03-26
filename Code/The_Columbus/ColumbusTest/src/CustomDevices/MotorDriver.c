9571961aa9cdd602edb1481d282d6993485fefc7
Henry Lovett <hl13g10@soton.ac.uk>
2013-03-26 18:40:18 +0000

    Added a hysteresis based state machine and sample continuously. Things Work!!

--- Code/The_Columbus/ColumbusTest/src/CustomDevices/MotorDriver.c
+++ Code/The_Columbus/ColumbusTest/src/CustomDevices/MotorDriver.c
@@ -7,14 +7,6 @@
 #include <asf.h>
 #include "CustomDevices/CustomDevices.h"
 #include <delay.h>
-//Camera
-/*#include "CustomDevices/OV7670.h"*/
-//I2C Mux
-/*#include "CustomDevices/PCA9542A.h"*/
-//MotorDriver
-/*#include "CustomDevices/MotorDriver.h"*/
-//SDCard
-/*#include "CustomDevices/SD_Card.h"*/
 
 
 
@@ -47,18 +39,9 @@ static void pwm_start_gc(void)
                 SCIF_GCCTRL_PLL0,
                 AVR32_SCIF_GC_NO_DIV_CLOCK,
                 0);
-//   scif_gc_setup(  AVR32_SCIF_GCLK_PWM,
-// 				  SCIF_GCCTRL_OSC0,
-// 				  AVR32_SCIF_GC_NO_DIV_CLOCK,
-// 				  0);
-  // Now enable the generic clock
   scif_gc_enable(AVR32_SCIF_GCLK_PWM);
 }
-// #  define EXAMPLE_PWM_L_PIN             AVR32_PWM_PWML_0_1_PIN
-// #  define EXAMPLE_PWM_L_FUNCTION        AVR32_PWM_PWML_0_1_FUNCTION
-// #  define EXAMPLE_PWM_H_PIN             AVR32_PWM_PWMH_0_1_PIN
-// #  define EXAMPLE_PWM_H_FUNCTION        AVR32_PWM_PWMH_0_1_FUNCTION
-// #  define EXAMPLE_PWM_CHANNEL_ID        0
+
 void Motor_Init()
 {
 	//Turn boths motors off
@@ -76,35 +59,27 @@ void Motor_Init()
 	Motor_Control.Left_State = STOP;
 	Motor_Control.Right_State = STOP;
 
-	avr32_pwm_channel_t pwm_channel = {{0}, // cmr
-											{0}, // cdty
-											{0}, // cdtyupd
-											{0}, // cprd
-											{0}, // cprdupd
-											{0}, // ccnt
-											{0}, // dt
-											{0}};// dtupd  ;  One channel config.
+	avr32_pwm_channel_t pwm_channel = {	{0}, // cmr
+										{0}, // cdty
+										{0}, // cdtyupd
+										{0}, // cprd
+										{0}, // cprdupd
+										{0}, // ccnt
+										{0}, // dt
+										{0}};// dtupd  ;  One channel config.
 /*	unsigned int channel_id;*/
 
 	// Start PLL for PWM
 	local_start_highfreq_clock();
 	// Start Enable Generic Clock with PLL as source clock
 	pwm_start_gc();
-	
-	
-// 	gpio_enable_module_pin(EXAMPLE_PWM_L_PIN, EXAMPLE_PWM_L_FUNCTION);
-// 	gpio_enable_module_pin(EXAMPLE_PWM_H_PIN, EXAMPLE_PWM_H_FUNCTION);
-// 	gpio_enable_module_pin(M0_PWM_H_PIN, M0_PWM_H_FUNCTION);
-// 	gpio_enable_module_pin(AVR32_PIN_PB10, AVR32_PWM_PWMH_1_1_FUNCTION); //PWM1 Low
 	gpio_enable_module_pin(ML_PWM_H_PIN, ML_PWM_H_FUNCTION);
-	gpio_enable_module_pin(MR_PWM_H_PIN, MR_PWM_H_FUNCTION); //PWM1 Low
-	//gpio_enable_module_pin(M1_PWM_H_PIN, M1_PWM_H_FUNCTION);
+	gpio_enable_module_pin(MR_PWM_H_PIN, MR_PWM_H_FUNCTION); 
 	// PWM controller configuration.
 	pwm_opt.diva = AVR32_PWM_DIVA_CLK_OFF;
 	pwm_opt.divb = AVR32_PWM_DIVB_CLK_OFF;
 	pwm_opt.prea = AVR32_PWM_PREA_CCK;
 	pwm_opt.preb = AVR32_PWM_PREB_CCK;
-	
 	pwm_opt.fault_detection_activated = false;
 	pwm_opt.sync_channel_activated    = true;
 	pwm_opt.sync_update_channel_mode  = PWM_SYNC_UPDATE_MANUAL_WRITE_MANUAL_UPDATE;
@@ -114,27 +89,21 @@ void Motor_Init()
 	pwm_opt.sync_channel_select[3]    = false;
 	pwm_opt.cksel                     = PWM_CKSEL_GCLK;
 	pwm_init(&pwm_opt);
-	
 	// Update the period
 	pwm_update_period_value(10);
-	
 	// Channel configuration
-	pwm_channel.CMR.dte   = 0;        // Enable Deadtime for complementary Mode
-	pwm_channel.CMR.dthi  = 0;        // Deadtime Inverted on PWMH
-	pwm_channel.CMR.dtli  = 0;        // Deadtime Not Inverted on PWML
-	pwm_channel.CMR.ces   = 0;        // 0/1 Channel Event at the End of PWM Period
-	pwm_channel.CMR.calg  = PWM_MODE_LEFT_ALIGNED;       // Channel mode.
-	pwm_channel.CMR.cpol  = PWM_POLARITY_HIGH;            // Channel polarity.
-	pwm_channel.CMR.cpre  = AVR32_PWM_CPRE_CCK;           // Channel prescaler.
-	pwm_channel.cdty      = 50;       // Channel duty cycle, should be < CPRD.
-	pwm_channel.cprd      = 200;       // Channel period.
+	pwm_channel.CMR.dte   = 0;							// Enable Deadtime for complementary Mode
+	pwm_channel.CMR.dthi  = 0;							// Deadtime Inverted on PWMH
+	pwm_channel.CMR.dtli  = 0;							// Deadtime Not Inverted on PWML
+	pwm_channel.CMR.ces   = 0;							// 0/1 Channel Event at the End of PWM Period
+	pwm_channel.CMR.calg  = PWM_MODE_LEFT_ALIGNED;      // Channel mode.
+	pwm_channel.CMR.cpol  = PWM_POLARITY_HIGH;          // Channel polarity.
+	pwm_channel.CMR.cpre  = AVR32_PWM_CPRE_CCK;         // Channel prescaler.
+	pwm_channel.cdty      = 50;							// Channel duty cycle, should be < CPRD.
+	pwm_channel.cprd      = 200;						// Channel period.
 	
-/*	channel_id = M0_PWM_CHANNEL_ID;*/
 	pwm_channel_init(ML_PWM_CHANNEL_ID, &pwm_channel); // Set channel configuration to channel 0
-	//pwm_start_channels((1 << channel_id));  // Start channel 0 & 1.
-/*	channel_id = M1_PWM_CHANNEL_ID; */
 	pwm_channel_init(MR_PWM_CHANNEL_ID, &pwm_channel); // Set channel configuration to channel 0
-	//pwm_start_channels((1 << channel_id));  // Start channel 0 & 1.
 	Analogue_Comparator_Init();
 }
 __attribute__((__interrupt__)) static void ACInterruptHandler(void)
@@ -195,50 +164,29 @@ void Analogue_Comparator_Init()
 	//Make it an interrupt
 	Disable_global_interrupt();
 	
-	//INTC_init_interrupts();
 	acifa_configure_hysteresis(&AVR32_ACIFA1, ACIFA_COMP_SELA, 2);
 	acifa_configure(&AVR32_ACIFA1,
-	ACIFA_COMP_SELA,
-	POT0_AC1AP1_INPUT,
-	SENSE0_AC1AN1_INPUT,
-	FOSC0);
+					ACIFA_COMP_SELA,
+					POT0_AC1AP1_INPUT,
+					SENSE0_AC1AN1_INPUT,
+					FOSC0);
 	
 	acifa_configure_hysteresis(&AVR32_ACIFA1, ACIFA_COMP_SELB, 2);
 	acifa_configure(&AVR32_ACIFA1,
-	ACIFA_COMP_SELB,
-	POT1_AC1BP1_INPUT,
-	SENSE1_AC1BN1_INPUT,
-	FOSC0);
+					ACIFA_COMP_SELB,
+					POT1_AC1BP1_INPUT,
+					SENSE1_AC1BN1_INPUT,
+					FOSC0);
 	
-	// 		//Reset Wheels
-	/*		Motor_Go(FORWARD);*/
-	//M0_IN1_CLR;
-	// 		M1_IN1_CLR;
-	// 		while(acifa_is_aca_inp_higher(&AVR32_ACIFA1) == false)
-	// 			;
-	// 		M0_IN1_CLR;
-	//
-	// 		M1_IN1_SET;
-	// 		while(!acifa_is_acb_inp_higher(&AVR32_ACIFA1))
-	// 			;
-	// 		M1_IN1_CLR;
-	
-	
-	//Motor_Go(S)
-	//acifa_enable_interrupt(&AVR32_ACIFA1, (1 << AVR32_ACIFA_ACBINT )| (1 << AVR32_ACIFA_ACAINT));//Enable ACBINT and ACAINT
-	ENABLE_ACA_INTERRUPT;
-	ENABLE_ACB_INTERRUPT;
-	AVR32_ACIFA1.ier = 3; //enable interrupts	
- 	//acifa_enable_interrupt_toggle(&AVR32_ACIFA1, ACIFA_COMP_SELA);
- 	//acifa_enable_interrupt_toggle(&AVR32_ACIFA1, ACIFA_COMP_SELB);
-	acifa_enable_interrupt_inp_lower(&AVR32_ACIFA1, ACIFA_COMP_SELA);
- 	acifa_enable_interrupt_inp_lower(&AVR32_ACIFA1, ACIFA_COMP_SELB);
+// 	acifa_enable_interrupt(&AVR32_ACIFA1, (1 << AVR32_ACIFA_ACBINT )| (1 << AVR32_ACIFA_ACAINT));//Enable ACBINT and ACAINT
+// 	AVR32_ACIFA1.ier = 3; //enable interrupts	
+// 	acifa_enable_interrupt_toggle(&AVR32_ACIFA1, ACIFA_COMP_SELA);
+// 	acifa_enable_interrupt_toggle(&AVR32_ACIFA1, ACIFA_COMP_SELB);
+// 	acifa_enable_interrupt_inp_lower(&AVR32_ACIFA1, ACIFA_COMP_SELA);
+// 	acifa_enable_interrupt_inp_lower(&AVR32_ACIFA1, ACIFA_COMP_SELB);
 	acifa_start(&AVR32_ACIFA1, (ACIFA_COMP_SELA|ACIFA_COMP_SELB));
 	
-	
-	
-	
-	INTC_register_interrupt(&ACInterruptHandler,AVR32_ACIFA1_IRQ ,AVR32_INTC_INT0);
+//	INTC_register_interrupt(&ACInterruptHandler,AVR32_ACIFA1_IRQ ,AVR32_INTC_INT0);
 	
 	Enable_global_interrupt();
 }
@@ -288,16 +236,113 @@ void Motor_Start(int Motors)
 		pwm_start_channels((1 << MOTOR_R));
 	}	
 }
+
+#define HYST_MAX	10
+#define SAMPLE_TIME	10
 void Motors_Execute()
 {
+	int Left_State = acifa_is_acb_inp_higher(&AVR32_ACIFA1);
+	int Right_State = acifa_is_aca_inp_higher(&AVR32_ACIFA1);
+	int Left_Hyst = 0;
+	int Right_Hyst = 0;
+	bool Left_OnTab = false;
+	bool Right_OnTab = false;
+	
 	while(Motors_Moving())
 	{
-		ENABLE_ACA_INTERRUPT;
-		ENABLE_ACB_INTERRUPT;
-		for(int i = 0; i < 750; i++)
+		///////////////////////////////////////////////////////
+		// LEFT MOTOR /////////////////////////////////////////
+		///////////////////////////////////////////////////////
+		Left_OnTab = acifa_is_acb_inp_higher(&AVR32_ACIFA1); //Read the status of the comparator
+		
+		if(Left_State == 0) //State Not on Tab
 		{
-			delay_ms(1);
+			LED5_CLR;
+			if(Left_OnTab == true)
+			{
+				Left_Hyst++;
+				if(Left_Hyst == HYST_MAX)//reached threshold, change state
+				{
+					Left_State = 1;
+					Motor_Control.Left_Count--; //Decrement the global counter
+					Left_Hyst = 0;
+					print_dbg("\n\rLeft Count Decrement");
+				}
+			}
+			else //Reset counter
+			{
+				Left_Hyst = 0;
+			}
 		}
+		else //State On Tab
+		{
+			LED5_SET;
+			if(Left_OnTab == true)//still on a tab
+			{
+				Left_Hyst = 0;//Reset counter
+			}
+			else
+			{
+				Left_Hyst ++;
+				if(Left_Hyst == HYST_MAX)//Reached Threshold. Change state
+				{
+					Left_State = 0;
+					Left_Hyst = 0;
+				}
+			}
+		}			
+		///////////////////////////////////////////////////////
+		// RIGHT MOTOR ////////////////////////////////////////
+		///////////////////////////////////////////////////////	
+		Right_OnTab = acifa_is_aca_inp_higher(&AVR32_ACIFA1); //Read the status of the comparator
+		
+		if(Right_State == 0) //State Not on Tab
+		{
+			LED6_CLR;
+			if(Right_OnTab == true)
+			{
+				Right_Hyst++;
+				if(Right_Hyst == HYST_MAX)//reached threshold, change state
+				{
+					Right_State = 1;
+					Motor_Control.Right_Count--; //Decrement the global counter
+					Right_Hyst = 0;
+					print_dbg("\n\rRight Count Decrement");
+				}
+			}
+			else //Reset counter
+			{
+				Right_Hyst = 0;
+			}
+		}
+		else //State On Tab
+		{
+			LED6_SET;
+			if(Right_OnTab == true)//still on a tab
+			{
+				Right_Hyst = 0;//Reset counter
+			}
+			else
+			{
+				Right_Hyst ++;
+				if(Right_Hyst == HYST_MAX)//Reached Threshold. Change state
+				{
+					Right_State = 0;
+					Right_Hyst = 0;
+				}
+			}
+		}
+		int temp = 0;
+		if(Motor_Control.Left_Count <= 0) //if we have reached the end of the movement on left wheel
+			temp |= MOTOR_L;
+		
+		if(Motor_Control.Right_Count <= 0)
+			temp |= MOTOR_R;
+		if(temp != 0)
+			Motor_Stop(temp); //Stop the Motor
+		
+		//Delay to keep a low sample time
+		delay_ms(SAMPLE_TIME);
 	}
 }
 void Motor_Stop(int Motors)
