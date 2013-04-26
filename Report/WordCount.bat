a60e29f24faf9a176f9512637b54586b930c235e
Henry Lovett <hl13g10@soton.ac.uk>
2013-04-26 12:14:03 +0100

    Sorted some tables in the appendix out.

--- Report/WordCount.bat
+++ Report/WordCount.bat
@@ -1,5 +1,2 @@
 texcount -inc Report.tex
-echo off
 texcount -v -html -inc Report.tex > count.html
-echo on
-pause
--- Report/make.bat
+++ Report/make.bat
@@ -3,6 +3,7 @@ makeindex report.nlo -s nomencl.ist -o report.nls
 bibtex report
 pdflatex -synctex=1 report.tex
 pdflatex -synctex=1 report.tex
-WordCount
+texcount -inc Report.tex
+texcount -v -html -inc Report.tex > count.html
 copy Report.pdf /Y C:\Users\hslovett\Dropbox\Report.pdf
 echo Complete!
